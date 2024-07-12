3018998f330d00880c2fb98387f9198638e2ecd9
Legarde Montcho <tchioidriss@yahoo.fr>
2024-07-12 14:43:43 +0200

    Update network.dart

---
+++ lib/core/utilities/network.dart
@@ -0,0 +1,31 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
+
+import '../../../locator.dart';
+import 'onoffline.dart';
+
+class UtilityNetwork {
+  static final UtilityNetwork _instance = UtilityNetwork._internal();
+  factory UtilityNetwork() => _instance;
+
+  late UtilityOnOffline _onOff;
+
+  UtilityNetwork._internal() {
+    _onOff = sl.get<UtilityOnOffline>();
+  }
+
+  bool getOnlineOfflineStatusWithConnectivity(ConnectivityResult connectionStatus) {
+    if (connectionStatus == ConnectivityResult.none) {
+      return false;
+    }
+
+    return _onOff.getOnlineOfflineStatus();
+  }
+
+  bool getOnlineOfflineTryStatusWithConnectivity(ConnectivityResult connectionStatus) {
+    if (connectionStatus == ConnectivityResult.none) {
+      return false;
+    }
+
+    return _onOff.getOnlineOfflineTryStatus();
+  }
+}
